1255cc4c8c9c2e8dbe8041271d270758a1f14fb5
erikmarto <dam2.emartorell@institutoserpis.org>
2017-10-31 12:31:55 +0100

    Menu nuevo ejercicio

---
+++ Menu/app/src/main/java/com/example/erigom/menu/Pantalla03.java
@@ -0,0 +1,16 @@
+package com.example.erigom.menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Pantalla03 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla03);
+        TextView lblMensaje =(TextView) findViewById(R.id.lblMensaje);
+        lblMensaje.setText ("TRIANGULO!");
+    }
+}
