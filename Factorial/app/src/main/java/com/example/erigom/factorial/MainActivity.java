ff849c342144c49a7f7e6347b926d689dc57a015
erikmarto <dam2.emartorell@institutoserpis.org>
2017-12-19 12:21:11 +0100

    Factorial acabado

--- Factorial/app/src/main/java/com/example/erigom/factorial/MainActivity.java
+++ Factorial/app/src/main/java/com/example/erigom/factorial/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.erigom.factorial;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button goToFactSinHilosButton;
+    Button goToFactConHilosButton;
+    Button goToFactConAsynctaskButton;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goToFactSinHilosButton = findViewById(R.id.activity_main_btn_fact_sin_hilos);
+        goToFactConHilosButton = findViewById(R.id.activity_main_btn_fact_con_hilos);
+        goToFactConAsynctaskButton = findViewById(R.id.activity_main_btn_fact_con_asyntask);
+
+        goToFactSinHilosButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FactorialSinHilosActivity.class);
+
+                goToActivity(intent);
+            }
+        });
+
+        goToFactConHilosButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FactorialConHilosActivity.class);
+
+                goToActivity(intent);
+            }
+        });
+
+        goToFactConAsynctaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FactorialConAsynctaskActivity.class);
+
+                goToActivity(intent);
+            }
+        });
+    }
+
+    private void goToActivity(Intent intent) {
+        startActivity(intent);
     }
 }
