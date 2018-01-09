00a9e0985c5146f939fb353e86272f78b89d735e
erikmarto <dam2.emartorell@institutoserpis.org>
2018-01-09 10:27:10 +0100

    FragmentoDinamico acabado

--- FragmentoDinamico/app/src/main/java/com/example/erigom/fragmentodinamico/MainActivity.java
+++ FragmentoDinamico/app/src/main/java/com/example/erigom/fragmentodinamico/MainActivity.java
@@ -1,52 +1,52 @@
 package com.example.erigom.fragmentodinamico;
-
-
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
     int mStackPosition = 1;
-    public void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // BotÃ³n de aÃ±adir fragments
-        Button button = (Button)findViewById(R.id.newFragment);
+        Button button =(Button)findViewById(R.id.newFragment);
         button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+            @Override
+            public void onClick(View view) {
                 addFragment();
             }
         });
+        if (savedInstanceState == null){
 
-        if (savedInstanceState == null) {
-            // aÃ±adir el primer fragment
-            Fragment newFragment = SimpleFragment.newInstance(mStackPosition);
-            FragmentTransaction ft = getFragmentManager().beginTransaction();
-            ft.add(R.id.fragmentShow, newFragment).commit();
-        } else {
+            Fragment fragment = SimpleFragment.newInstance(mStackPosition);
+            FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.fragmentShow,fragment).commit();
+
+        }else
+        {
             mStackPosition = savedInstanceState.getInt("position");
         }
     }
+    public void addFragment(){
+        mStackPosition ++;
+        Fragment fragment = SimpleFragment.newInstance(mStackPosition);
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentShow,fragment);
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
 
-    void addFragment() {
-        mStackPosition++;
-        // Instanciamos nuevo Fragment
-        Fragment newFragment = SimpleFragment.newInstance(mStackPosition);
-        // Se aÃ±ade el Fragment a la actividad
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
-        ft.replace(R.id.fragmentShow, newFragment);
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        // aÃ±adimos la transaciÃ³n a la pila
-        ft.addToBackStack(null);
-        ft.commit();
     }
+
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putInt("position", mStackPosition);
+        outState.putInt("position",mStackPosition);
     }
-
 }
--- FragmentoDinamico/app/src/main/java/com/example/erigom/fragmentodinamico/SimpleFragment.java
+++ FragmentoDinamico/app/src/main/java/com/example/erigom/fragmentodinamico/SimpleFragment.java
@@ -1,8 +1,5 @@
 package com.example.erigom.fragmentodinamico;
-
 import android.app.Fragment;
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,39 +7,35 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SimpleFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SimpleFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SimpleFragment extends Fragment {
     int mNum;
-    static SimpleFragment newInstance(int number) {
+    public static SimpleFragment newInstance(int number){
         SimpleFragment f = new SimpleFragment();
         Bundle args = new Bundle();
-        args.putInt("num", number);
+        args.putInt("num",number);
         f.setArguments(args);
         return f;
     }
-    public void onCreate(Bundle savedInstanceState) {
+
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mNum = getArguments().getInt("num");
     }
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v  = null;
-        View tv = null;
-        if (mNum % 2 == 0){
-            v = inflater.inflate(R.layout.fragment_simple, container, false);
-            tv = v.findViewById(R.id.text);            }
-        else{
-            v = inflater.inflate(R.layout.fragment_simple2 , container, false);
-            tv = v.findViewById(R.id.text2);            }
 
-        ((TextView)tv).setText("Fragmento nÃºmero #" + mNum);
+    @Override
+    public View onCreateView(LayoutInflater layoutInflater,ViewGroup container, Bundle savedInstanceState) {
+        View v = null;View tv;
+
+        if (mNum % 2 == 0){
+            v = layoutInflater.inflate(R.layout.fragment_simple,container,false);
+            tv = v.findViewById(R.id.text);
+        }
+        else{
+            v = layoutInflater.inflate(R.layout.fragment_simple2,container,false);
+            tv = v.findViewById(R.id.text2);
+        }
+        ((TextView)tv).setText("Fragmento numero "+ mNum);
         return v;
     }
 }
